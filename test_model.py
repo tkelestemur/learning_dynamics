3d758f15dc96e1c2a13da27b32b7d0414ca038ba
Tarik Kelestemur <tkelestemur@gmail.com>
2019-04-24 10:43:46 -0400

    test model updated

--- test_model.py
+++ test_model.py
@@ -24,12 +24,12 @@ def trajectory_prediction():
     fig.set_size_inches(12, 8)
     fig.suptitle('Pendulum')
     ax.set_xlabel('Timestep')
-    ax.set_ylabel('Pole Angle')
+    ax.set_ylabel('State')
 
     # Loadt test set
     pend_test_data = PendulumDataset('valid')
 
-    states, actions = pend_test_data[100]
+    states, actions = pend_test_data[0]
 
     for j, checkpoint in enumerate(checkpoints):
         model.load_state_dict(torch.load(str(checkpoint), map_location=torch.device('cpu')), strict=True)
@@ -51,12 +51,15 @@ def trajectory_prediction():
         #     with torch.no_grad():
         #         state_encoded = model.transform(state_encoded, actions[i])
         #         state_decoded = model.f_decoder(state_encoded)
-        #         print(state_decoded.shape)
+        #         # print(state_decoded.shape)
         #         states_net[0, i+1] = state_decoded[:, -1, :]
 
         states_net = states_net.view(200, 3).numpy()
-        ax.plot(states_net[:, 0], '--', label=str(j+1) + '-step loss', linewidth=2)
-    ax.plot(states_sim[:, 0], c='r', label='true state', linewidth=2)
+        ax.plot(states_net[:, 0], '--', label=str(j+1) + '-step loss [angle]', linewidth=2)
+        ax.plot(states_net[:, 2], '--', label=str(j + 1) + '-step loss [velocity]', linewidth=2)
+        # ax.plot(states_net[:, 0], '--', label=str(j + 1) + '-step loss', linewidth=2)
+    ax.plot(states_sim[:, 0], c='r', label='true state [angle]', linewidth=2)
+    ax.plot(states_sim[:, 2], c='b', label='true state [velocity]', linewidth=2)
 
     plt.legend()
     plt.show()
@@ -66,7 +69,7 @@ def calculate_mse():
     # Load the model
     checkpoint_path = './checkpoints/lstm_auto_encoder/checkpoint_16h_1step.pth'
     model = LSTMAutoEncoder(input_size=3, action_size=1, hidden_size=16, num_layers=1, bias=True, k_step=3)
-    model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cuda')), strict=True)
+    model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cpu')), strict=True)
     model.eval()
 
     fig, ax = plt.subplots(1, 1)
@@ -77,7 +80,7 @@ def calculate_mse():
 
     pend_test_data = PendulumDataset('valid')
 
-    num_samples = 1000
+    num_samples = 1
     prediction_error = np.zeros((num_samples, pend_test_data.data.shape[1]))
     for j in tqdm(range(num_samples)):
         states, actions = pend_test_data[j]
@@ -97,10 +100,13 @@ def calculate_mse():
         states_net = states_net.view(200, 3).numpy()
         prediction_error[j] = (np.square(states_sim - states_net)).mean(axis=1)
 
-    prediction_error_mean = prediction_error.mean(axis=0)
-    np.save('./results/checkpoint_16h_1step_mse', prediction_error)
-    ax.plot(prediction_error_mean, c='r', label='prediction error', linewidth=2)
-    plt.show()
+    print(states_net[2])
+    print(states_sim[2])
+    print(prediction_error[0, 2])
+    # prediction_error_mean = prediction_error.mean(axis=0)
+    # np.save('./results/checkpoint_16h_1step_mse', prediction_error)
+    # ax.plot(prediction_error_mean, c='r', label='prediction error', linewidth=2)
+    # plt.show()
 
 
 if __name__ == '__main__':
